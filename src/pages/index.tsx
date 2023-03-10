43cdd17b91ecdee75ea2954d22a9bcaac9a78823
Denilson <dlemus@harding.edu>
2023-03-10 14:57:56 -0600

    📱 Add main content width constraint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header/Header";
+import Banner from "@/components/Banner/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Header />
+      <Header />
+      <main className="max-w-screen-2xl m-auto">
+        <Banner />
       </main>
     </>
   );
