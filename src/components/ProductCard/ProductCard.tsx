e600df32546fc12dcf5f277d2eb764f6024d1f10
Denilson <dlemus@harding.edu>
2023-03-13 12:28:23 -0600

    ✨ Add rating functionality

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -5,12 +5,23 @@ type ProductCardProps = {
 };
 
 function ProductCard({ product }: ProductCardProps) {
+  const stars = [
+    <IconStarFilled key={product.id} />,
+    <IconStar key={product.id} />,
+  ];
+
+  const rating = Math.floor(Math.random() * 5) + 1;
+
+  const starRating = Array(5).fill(<IconStar />);
+  starRating.fill(<IconStarFilled />, 0, rating);
+
   return (
     <div>
       <img src={product.image} width={200} height={200} alt={product.title} />
       <h4>{product.title}</h4>
-      <IconStarFilled />
-      <IconStar />
+      {starRating.map((icon, index) => (
+        <span key={index}>{icon}</span>
+      ))}
     </div>
   );
 }
