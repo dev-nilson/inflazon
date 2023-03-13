1ea0b3c07ee97d54165886b1ecb72ed6bae23e13
Denilson <dlemus@harding.edu>
2023-03-13 12:10:49 -0600

    🚨 Fix key issue for products

--- src/data.ts
+++ src/data.ts
@@ -1,15 +1,18 @@
 export const products = [
   {
+    id: 1,
     title: "Title1",
     price: 10,
     image: "https://joadre.com/wp-content/uploads/2019/02/no-image.jpg"
   },
   {
+    id: 2,
     title: "Title2",
     price: 20,
     image: "https://joadre.com/wp-content/uploads/2019/02/no-image.jpg"
   },
   {
+    id: 3,
     title: "Title3",
     price: 30,
     image: "https://joadre.com/wp-content/uploads/2019/02/no-image.jpg"
