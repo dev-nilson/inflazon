44dc8a1318986524dbe2620f4648a4412558b09b
Denilson <dlemus@harding.edu>
2023-03-13 12:04:38 -0600

    🤡 Mock data for products

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    title: "Title1",
+    price: 10,
+    image: "https://joadre.com/wp-content/uploads/2019/02/no-image.jpg"
+  },
+  {
+    title: "Title2",
+    price: 20,
+    image: "https://joadre.com/wp-content/uploads/2019/02/no-image.jpg"
+  },
+  {
+    title: "Title3",
+    price: 30,
+    image: "https://joadre.com/wp-content/uploads/2019/02/no-image.jpg"
+  },
+]
