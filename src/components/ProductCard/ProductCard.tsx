a91bcc3697721d464c97dad770154461e0adc650
Denilson <dlemus@harding.edu>
2023-03-13 14:09:51 -0600

    ✨ Add prodcuts

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -17,17 +17,20 @@ function ProductCard({ product }: ProductCardProps) {
   starRating.fill(<IconStarFilled />, 0, rating);
 
   return (
-    <div className="">
-      <img src={product.image} width={200} height={200} alt={product.title} />
-      <h4>{product.title}</h4>
-      <div>
+    <div className="relative flex flex-col m-5 bg-white z-30">
+      <img className="m-auto" src={product.image} width={200} height={200} alt={product.title} />
+      <h3 className="text-md px-2 pt-2 pb-1">{product.title}</h3>
+      <div className="flex px-2">
         {starRating.map((icon, index) => (
-          <span key={index}>{icon}</span>
+          <span className="text-amazonYellow-dark" key={index}>
+            {icon}
+          </span>
         ))}
       </div>
-      <div>
-          <Currency quantity={product.price} currency="USD" />
+      <div className="text-2xl p-2 font-semibold">
+        <Currency quantity={product.price} currency="USD" />
       </div>
+      <button className="mt-auto mb-4 mx-4 button">Add to Cart</button>
     </div>
   );
 }
--- src/components/ProductFeed/ProductFeed.tsx
+++ src/components/ProductFeed/ProductFeed.tsx
@@ -5,7 +5,7 @@ function ProductFeed() {
   console.log(products);
 
   return (
-    <div>
+    <div className="grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:-mt-52">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
