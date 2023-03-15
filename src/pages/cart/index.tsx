d813f0cb43de5688e205319b39a43ac822dbb186
Denilson <dlemus@harding.edu>
2023-03-15 12:33:00 -0600

    📱 Work on cart responsiveness

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import Currency from "react-currency-formatter";
 import { selectItems } from "@/redux/slices/cartSlice";
 import Header from "@/components/Header/Header";
 import CartCard from "@/components/CartCard/CartCard";
@@ -9,8 +10,8 @@ function CartPage() {
   return (
     <div>
       <Header />
-      <main className="lg:flex max-w-screen-2xl mx-auto">
-        <section className="flex-grow m-5">
+      <main className="lg:flex max-w-screen-2xl">
+        <section className="lg:w-3/4 m-5">
           <div className="flex flex-col p-5 space-y-10 bg-white">
             <h1 className="text-2xl font-semibold border-b pb-4">
               {items.length === 0 ? "Cart is Empty" : "Order Details"}
@@ -20,7 +21,16 @@ function CartPage() {
             ))}
           </div>
         </section>
-        <section></section>
+        <section className="lg:w-1/4 m-5">
+          {items.length > 0 && (
+            <div className="flex flex-col bg-white p-5">
+              <h2>Order Summary</h2>
+              <span>
+              </span>
+              <button className="button">Place your order</button>
+            </div>
+          )}
+        </section>
       </main>
     </div>
   );
