30e59cfabcf8a75a72fc2c2797a43c8aee64ad7b
Denilson <dlemus@harding.edu>
2023-03-26 23:46:28 -0600

    🔥 Remove console statements

--- src/components/ProductFeed/ProductFeed.tsx
+++ src/components/ProductFeed/ProductFeed.tsx
@@ -2,8 +2,6 @@ import { products } from "../../data";
 import ProductCard from "../ProductCard/ProductCard";
 
 function ProductFeed() {
-  console.log(products);
-
   return (
     <div className="grid grid-flow-row-dense sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:-mt-52">
       {products.map((product) => (
