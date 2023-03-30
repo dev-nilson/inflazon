c03489100ba93826a61ea270222399c30d4aa2d4
Denilson <dlemus@harding.edu>
2023-03-29 19:03:32 -0600

    🗃️ Update products data

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,7 @@
 import bigmac from "../assets/bigmac.jpg";
 import coke from "../assets/coke.jpg";
+import water from "../assets/water.jpg";
+import nike from "../assets/nike.jpg";
 
 export const products = [
   {
@@ -15,5 +17,19 @@ export const products = [
     newPrice: 10,
     oldPrice: 1,
     image: coke,
-  }
+  },
+  {
+    id: 2,
+    title: "Water Bottle",
+    newPrice: 10,
+    oldPrice: 1,
+    image: water,
+  },
+  {
+    id: 3,
+    title: "Nike Air Force",
+    newPrice: 10,
+    oldPrice: 1,
+    image: nike,
+  },
 ];
