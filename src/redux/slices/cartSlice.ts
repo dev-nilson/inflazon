1bc24678801c89787efcd2dd342c7e356ecaf5b3
Denilson <dlemus@harding.edu>
2023-04-18 14:29:37 -0600

    🚨 Fix type warnings

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -8,19 +8,19 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItem: (state, action) => {
+    addItem: (state: Cart, action) => {
       state.items = [...state.items, action.payload];
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
+      state.items = state.items.filter((item: Product) => item.id !== action.payload);
     },
   },
 });
 
 export const { addItem, removeItem } = cartSlice.actions;
-export const selectItems = (state: any) => state.cart.items;
-export const selectNewTotal = (state: any) =>
+export const selectItems = (state: State) => state.cart.items;
+export const selectNewTotal = (state: State) =>
   state.cart.items.reduce((total, item) => total + item.newPrice, 0);
-export const selectOldTotal = (state: any) =>
+export const selectOldTotal = (state: State) =>
   state.cart.items.reduce((total, item) => total + item.oldPrice, 0);
 export default cartSlice.reducer;
